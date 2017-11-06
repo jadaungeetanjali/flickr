009d18b8e58fa5ff8116fa0118e5302944236626
deepanshut041 <deepanshut041@gmail.com>
2017-11-06 19:26:56 +0530

    Moviecast config made

---
+++ app/src/main/java/com/example/pc/flickr/utilities/MovieCastJsonConfig.java
@@ -0,0 +1,11 @@
+package com.example.pc.flickr.utilities;
+
+/**
+ * Created by Deepanshu on 11/6/2017.
+ */
+public class MovieCastJsonConfig {
+    public static final String CAST_ID = "cast_id";
+    public static final String CHARACTER = "character";
+    public static final String CAST_NAME = "name";
+    public static final String CAST_IMAGE_URL = "profile_path";
+}
